5992a24c7b32399be49580b17425ccb37c92d30f
lizbethMelissaPS <melissasud27@gmail.com>
2023-10-12 19:36:11 -0500

    :bug: corrigiendo error cors

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const cors = require ('cors');
 const alumnoRoutes = require('../routes/alumno.routes');
-
+const cursoRoutes = require('../routes/curso.routes');
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(cors());
 
 // endpoints
 app.use('/alumno', alumnoRoutes);
-
+app.use('/curso', cursoRoutes);
 module.exports = app;
